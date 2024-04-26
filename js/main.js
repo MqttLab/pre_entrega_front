6239308c194a6980ba1e7e842e29dff3e011860e
MqttLab <maurimaldonado6@gmail.com>
2024-04-26 16:47:45 -0300

    console.log('Test')

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-const url = "https://api.wit.ai/message?v=20140826&q=";
-
-const options = {
-  headers: {
-    Authorization: "Bearer 6Q************"
-  }
-};
-
-fetch(url, options)
-  .then( res => res.json() )
-  .then( data => console.log(data) );
+console.log("Test");
